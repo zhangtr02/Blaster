c4740ff5e42ddcd87ea329f84ad4b863632eec12
Tianrong Zhang <zhangtr02@gmail.com>
2025-12-06 15:39:02 +0800

    Seamless Travel and Lobby

---
+++ Source/Blaster/GameMode/LobbyGameMode.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+public:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+};
